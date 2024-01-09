361adfc93ec779cff2da7302314439d57c0106f2
akki2605 <akashtripathi0802@gmail.com>
2024-01-09 11:07:05 +0530

    submit for funcnality added and thank you text added

--- js/main.js
+++ js/main.js
@@ -19,3 +19,44 @@ function toggleAnswer(element) {
     faqAnswer.style.maxHeight = '0';
   }
 }
+
+
+window.addEventListener("load", function() {
+  const form = document.getElementById('enquire-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      // // Hide first view
+      const leadForm = document.getElementById('lead-form');
+
+      setTimeout(() => {
+        leadForm.style.display = 'none';
+
+        // Show thank you message element with a delay and transition
+        const leadFormSubmitted = document.getElementById('lead-form-submitted');
+        leadFormSubmitted.style.transition = 'opacity 1s';
+        leadFormSubmitted.style.opacity = '1';
+        leadFormSubmitted.style.display = 'block';
+      }, 500);
+      
+    })
+  });
+});
+
+function closeForm() {
+  // Hide the thank-you message
+  document.getElementById('lead-form-submitted').style.display = 'none';
+
+  // Show the initial form
+  var leadForm = document.getElementById('lead-form');
+  leadForm.style.display = 'block';
+
+  document.getElementById('enquire-form').reset();
+}
+
